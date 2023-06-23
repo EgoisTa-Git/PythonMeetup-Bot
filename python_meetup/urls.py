973eb98557dc441fd1cb1236c50f8864a28c9f92
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-06-23 17:15:41 +0300

    Changed site header, title and index title

--- python_meetup/urls.py
+++ python_meetup/urls.py
@@ -18,6 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.admin.sites import all_sites
 
+admin.site.site_header = 'Python Meetup'
+admin.site.site_title = 'Python Meetup'
+admin.site.index_title = 'Python Meetup administration'
+
 
 urlpatterns = [
     path(f'{site.name}/', site.urls) for site in all_sites
