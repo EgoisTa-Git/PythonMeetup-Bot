4a9b3991799b7450a6babfd80b3b18df40966d55
EgoisTa <97385729+EgoisTa-Git@users.noreply.github.com>
2023-06-25 02:48:32 +0300

    Add questions dialog and simplify bot core

--- meetup_bot/management/commands/startbot.py
+++ meetup_bot/management/commands/startbot.py
@@ -1,28 +1,10 @@
 """Менеджмент команда запуска бота"""
 
-from django.conf import settings
 from django.core.management import BaseCommand
 
-from meetup_bot.tg_bot_main import TGBot, handle_role, start, handle_menu, show_menu
-
-from meetup_bot.guests_chat import invite_to_chat, handle_chat_invite
+from meetup_bot.tg_bot_main import bot
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        try:
-            tg_bot = TGBot(
-                settings.TG_BOT_APIKEY,
-                {
-                    'START': start,
-                    'HANDLE_ROLE': handle_role,
-                    'SHOW_MENU': show_menu,
-                    'HANDLE_MENU': handle_menu,
-                    'CHAT_INVITE': invite_to_chat,
-                    'HANDLE_INVITE': handle_chat_invite,
-                }
-            )
-            tg_bot.updater.start_polling()
-            tg_bot.updater.idle()
-        except Exception as err:
-            print(err)
+        bot()
