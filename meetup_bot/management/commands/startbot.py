6825e3f96d4fb0feb41777732e1126001af31e13
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-06-26 14:06:42 +0300

    Add logging startbot.py

--- meetup_bot/management/commands/startbot.py
+++ meetup_bot/management/commands/startbot.py
@@ -1,10 +1,20 @@
 """Менеджмент команда запуска бота"""
+import logging
 from django.conf import settings
 from django.core.management import BaseCommand
 
 from meetup_bot.tg_bot_main import TGBot, handle_menu
 from meetup_bot.services import start, handle_role, handle_donate, handle_message, write_speaker_from_schedule, \
     start_poll, handle_poll_answer
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+log_format = logging.Formatter(
+    fmt='{asctime} - {levelname} - {name} - {message}',
+    style='{')
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(log_format)
+logger.addHandler(stream_handler)
 
 
 class Command(BaseCommand):
@@ -26,4 +36,4 @@ class Command(BaseCommand):
             tg_bot.updater.start_polling()
             tg_bot.updater.idle()
         except Exception as err:
-            print(err)
+            logger.error(err, exc_info=True)
